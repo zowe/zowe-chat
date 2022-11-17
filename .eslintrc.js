918d845c13e56c0a0be87041be86490724ed95e5
MarkAckert <mark.ackert@broadcom.com>
2022-11-17 16:25:58 -0500

    chore(lint): update linting rules
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,95 +1,118 @@
 /*
-* This program and the accompanying materials are made available under the terms of the
-* Eclipse Public License v2.0 which accompanies this distribution, and is available at
-* https://www.eclipse.org/legal/epl-v20.html
-*
-* SPDX-License-Identifier: EPL-2.0
-*
-* Copyright Contributors to the Zowe Project.
-*/
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 
 module.exports = {
-    'env': {
-        'browser': false,
-        'node': true,
-        'es6': true,
-        'jest': true,
+    env: {
+        browser: false,
+        node: true,
+        es6: true,
+        jest: true,
     },
-    'parser': '@typescript-eslint/parser',
-    'plugins': [
-        '@typescript-eslint',
-        'header',
-    ],
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'google',
-    ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly',
-    },
-    'parserOptions': {
-        'ecmaVersion': 11,
-        'sourceType': 'module',
-    },
-    'ignorePatterns': ['**/dist', '**/node_modules', '**/release', '**/lib'],
-    'rules': {
-        'header/header': [2,
+    plugins: ['node', 'prettier', 'header'],
+    extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
+    ignorePatterns: ['**/.build', '**/build', '**/dist', '**/node_modules', '**/release', '**/lib'],
+    rules: {
+        'header/header': [
+            2,
             'block',
-            ['\n* This program and the accompanying materials are made available under the terms of the'
-            + '\n* Eclipse Public License v2.0 which accompanies this distribution, and is available at'
-            + '\n* https://www.eclipse.org/legal/epl-v20.html'
-            + '\n*'
-            + '\n* SPDX-License-Identifier: EPL-2.0'
-            + '\n*'
-            + '\n* Copyright Contributors to the Zowe Project.\n'],
+            [
+                '\n * This program and the accompanying materials are made available under the terms of the' +
+                    '\n * Eclipse Public License v2.0 which accompanies this distribution, and is available at' +
+                    '\n * https://www.eclipse.org/legal/epl-v20.html' +
+                    '\n *' +
+                    '\n * SPDX-License-Identifier: EPL-2.0' +
+                    '\n *' +
+                    '\n * Copyright Contributors to the Zowe Project.' +
+                    '\n ',
+            ],
             2,
         ],
-        'indent': [
-            'error', 4, {
-                'CallExpression': {
-                    'arguments': 2,
-                },
-                'FunctionDeclaration': {
-                    'body': 1,
-                    'parameters': 2,
-                },
-                'FunctionExpression': {
-                    'body': 1,
-                    'parameters': 2,
-                },
-                'MemberExpression': 2,
-                'ObjectExpression': 1,
-                'SwitchCase': 1,
-                'ignoredNodes': [
-                    'ConditionalExpression',
-                ],
+        'prettier/prettier': 'error',
+        'block-scoped-var': 'error',
+        eqeqeq: ['error', 'always', {null: 'ignore'}],
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'eol-last': 'error',
+        'prefer-arrow-callback': 'error',
+        'no-trailing-spaces': 'error',
+        quotes: ['warn', 'single', {avoidEscape: true}],
+        'no-restricted-properties': [
+            'error',
+            {
+                object: 'describe',
+                property: 'only',
+            },
+            {
+                object: 'it',
+                property: 'only',
             },
         ],
-        'max-len': ['error', {
-            'code': 160,
-            'tabWidth': 2,
-            'ignoreUrls': true,
-            'ignoreTemplateLiterals': true,
-        }],
-        'object-curly-spacing': ['error', 'always'],
-        'operator-linebreak': ['error', 'before'],
-        'require-jsdoc': ['off', {
-            require: {
-                FunctionDeclaration: true,
-                MethodDefinition: true,
-                ClassDeclaration: true,
-            },
-        }],
-        'valid-jsdoc': ['off', {
-            requireParamDescription: false,
-            requireReturnDescription: false,
-            requireReturn: false,
-            prefer: { returns: 'return' },
-        }],
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/no-inferrable-types': 'off',
     },
+    overrides: [
+        {
+            files: ['**/*.ts', '**/*.tsx'],
+            parser: '@typescript-eslint/parser',
+            extends: ['plugin:@typescript-eslint/recommended', 'google'],
+            rules: {
+                '@typescript-eslint/no-non-null-assertion': 'off',
+                '@typescript-eslint/no-use-before-define': 'off',
+                '@typescript-eslint/no-warning-comments': 'off',
+                '@typescript-eslint/no-empty-function': 'off',
+                '@typescript-eslint/no-var-requires': 'off',
+                '@typescript-eslint/explicit-function-return-type': 'off',
+                '@typescript-eslint/explicit-module-boundary-types': 'off',
+                '@typescript-eslint/ban-types': 'off',
+                '@typescript-eslint/camelcase': 'off',
+                indent: ['error', 4, {SwitchCase: 1}],
+                'max-len': [
+                    'error',
+                    {
+                        code: 140,
+                        comments: 160,
+                        tabWidth: 4,
+                        ignoreUrls: true,
+                        ignoreTemplateLiterals: true,
+                    },
+                ],
+
+                'node/no-missing-import': 'off',
+                'node/no-empty-function': 'off',
+                'node/no-unsupported-features/es-syntax': 'off',
+                'node/no-missing-require': 'off',
+                'node/shebang': 'off',
+                'no-dupe-class-members': 'off',
+                'require-atomic-updates': 'off',
+                'require-jsdoc': [
+                    'off',
+                    {
+                        require: {
+                            FunctionDeclaration: true,
+                            MethodDefinition: true,
+                            ClassDeclaration: true,
+                        },
+                    },
+                ],
+                'valid-jsdoc': [
+                    'off',
+                    {
+                        requireParamDescription: false,
+                        requireReturnDescription: false,
+                        requireReturn: false,
+                        prefer: {returns: 'return'},
+                    },
+                ],
+            },
+            parserOptions: {
+                ecmaVersion: 2018,
+                sourceType: 'module',
+            },
+        },
+    ],
 };
--- packages/clicmd/.eslintrc.js
+++ packages/clicmd/.eslintrc.js
@@ -1,95 +1,13 @@
 /*
-* This program and the accompanying materials are made available under the terms of the
-* Eclipse Public License v2.0 which accompanies this distribution, and is available at
-* https://www.eclipse.org/legal/epl-v20.html
-*
-* SPDX-License-Identifier: EPL-2.0
-*
-* Copyright Contributors to the Zowe Project.
-*/
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 
 module.exports = {
-    'env': {
-        'browser': false,
-        'node': true,
-        'es6': true,
-        'jest': true,
-    },
-    'parser': '@typescript-eslint/parser',
-    'plugins': [
-        '@typescript-eslint',
-        'header',
-    ],
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'google',
-    ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly',
-    },
-    'parserOptions': {
-        'ecmaVersion': 11,
-        'sourceType': 'module',
-    },
-    'ignorePatterns': ['**/dist', '**/node_modules', '**/release', '**/lib'],
-    'rules': {
-        'header/header': [2,
-            'block',
-            ['\n* This program and the accompanying materials are made available under the terms of the'
-            + '\n* Eclipse Public License v2.0 which accompanies this distribution, and is available at'
-            + '\n* https://www.eclipse.org/legal/epl-v20.html'
-            + '\n*'
-            + '\n* SPDX-License-Identifier: EPL-2.0'
-            + '\n*'
-            + '\n* Copyright Contributors to the Zowe Project.\n'],
-            2
-        ],
-        'indent': [
-            'error', 4, {
-                'CallExpression': {
-                    'arguments': 2,
-                },
-                'FunctionDeclaration': {
-                    'body': 1,
-                    'parameters': 2,
-                },
-                'FunctionExpression': {
-                    'body': 1,
-                    'parameters': 2,
-                },
-                'MemberExpression': 2,
-                'ObjectExpression': 1,
-                'SwitchCase': 1,
-                'ignoredNodes': [
-                    'ConditionalExpression',
-                ],
-            },
-        ],
-        'max-len': ['error', {
-            code: 160,
-            tabWidth: 2,
-            ignoreUrls: true,
-            ignorePattern: 'goog\.(module|require)',
-        }],
-        'object-curly-spacing': ['error', 'always'],
-        'operator-linebreak': ['error', 'before'],
-        'require-jsdoc': ['off', {
-            require: {
-                FunctionDeclaration: true,
-                MethodDefinition: true,
-                ClassDeclaration: true,
-            },
-        }],
-        'valid-jsdoc': ['off', {
-            requireParamDescription: false,
-            requireReturnDescription: false,
-            requireReturn: false,
-            prefer: {returns: 'return'},
-        }],
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/no-inferrable-types': 'off',
-    },
+    ...require('../../.eslintrc.js'),
 };
